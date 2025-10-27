330ad8ed3ffcffb3a210ccbbdb14b371425b6b2e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-27 11:55:03 +0700

    index.js

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ app.use(
   })
 );
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
 
-db.sequelize.sync().then((result) => {
-  app
-    .listen(3000, () => {
+db.sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000, () => {
       console.log("Server started");
-    })
-    .catch((err) => {
-      console.log(err);
     });
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
