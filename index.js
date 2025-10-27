e853454a5e9e92c2c76c576755d80ed7f6fb6bd8
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-27 12:16:10 +0700

    POST, GET, PUT

--- index.js
+++ index.js
@@ -2,24 +2,60 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const db = require("./models");
+const komik = require("./models/komik");
 app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: true,
+    extended: false,
   })
 );
 
-app.listen(PORT, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
 });
 
 db.sequelize
   .sync()
   .then((result) => {
     app.listen(3000, () => {
-      console.log("Server started");
+      console.log("server started");
     });
   })
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/komiks", async (req, res) => {
+  const data = req.body;
+  try {
+    const komik = await db.Komik.create(data);
+    res.send(komik);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create komik" });
+  }
+});
+
+app.get("/komiks", async (req, res) => {
+  try {
+    const komiks = await db.Komik.findAll();
+    res.send(komiks);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.put("/komiks/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  try {
+    const komiks = await db.Komik.findByPk(id);
+    if (!komik) {
+      return res.status(404).json({ error: "Komik tidak tersedia" });
+    }
+    await komik.update(data);
+    res.send({ message: "Komik berhasil diupdate", komik });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+});
